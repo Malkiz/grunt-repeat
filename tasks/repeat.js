56651f364cb66e7b7fe3611cb3fbde50e5fea340
omri <omri.ey@walkme.com>
2015-02-17 09:30:32 +0200

    support more options variations

--- tasks/repeat.js
+++ tasks/repeat.js
@@ -2,9 +2,20 @@ module.exports = function(grunt) {
 	grunt.registerMultiTask('repeat', 'repeat a task several times', function(){
 		var _this = this;
 		var options = _this.options();
+		
+		// Set the tasks based on the config format
+		var tasks = _this.data.tasks || options.tasks || _this.data;
+
+		if (!tasks) {
+			tasks = [];
+		} else if (typeof tasks != 'object' || !tasks.length) {
+			tasks = [tasks];
+		}
 
 		for (var i = 0; i < options.times; i++) {
-			grunt.task.run(options.task);
+			tasks.forEach(function (task) {
+				grunt.task.run(task);
+			});
 		}
 	});
 }
